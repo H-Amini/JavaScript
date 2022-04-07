b96eedecfaf9df596bcd616e32280f8b979377ee
H-Amini <hosein202150@gmail.com>
2022-04-07 11:56:14 +0330

    Add Project

---
+++ maseg.js
@@ -0,0 +1,17 @@
+function msg() {
+  alert("Hello Hosein Amini");
+}
+var a = 10;
+var b = 20;
+var c = a + b; //it sum values
+document.write(c);
+
+var x = 10;
+if (10 < 13) {
+  var y = 20;
+  console.log(y);
+}
+var data = 200;
+function a() {
+  document.writeln(data);
+}
